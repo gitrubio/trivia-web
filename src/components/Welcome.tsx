ed3c3abb5b628e8758e654f3808093d6631eccc5
carlos rubio <86839953+gitrubio@users.noreply.github.com>
2023-02-24 21:15:11 -0500

    asegurando cambios

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,24 +1,32 @@
-import React from "react";
-import { Typography, Layout , Button, Card ,Row ,Col} from "antd";
-import Logo from '../assets/trivia-logo.png'
+import React, { useState } from "react";
+import { Typography, Layout , Button, Card } from "antd";
+import Logo from '../assets/logo.png'
 import "../styles/Welcome.css";
 const { Title } = Typography;
 const {  Content } = Layout;
 export default function Welcome() {
-  
+      const [loading, setloading] = useState(false)
+      
+      const getData = () => {
+        setloading(true)
+      
+      }
+
       return (
         <Layout>
           <Content className="container">
             <Card
              className="card"
               cover={
-                <img
+                <img style={{ maxWidth: 290}}
                   alt="example"
                   src={Logo}
                 />
               }
               actions={[
-    
+                <Button type="primary" loading={loading} onClick={getData}>
+                Click me!
+              </Button>
               ]}
             >
             {/*   <Meta
